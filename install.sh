1e74d914a8edd2782ab96160cc86c9782ca4bae1
Roman Sindarovskii <rsindar@yahoo.com>
2023-01-26 16:40:58 +0100

    Adding packages installation steps

--- install.sh
+++ install.sh
@@ -7,16 +7,40 @@ then
     exit 1
 fi
 
+dnf=$(command -v dnf)
+apt=$(command -v apt)
+if [ "$dnf" ]
+then
+    package_manager=$dnf
+elif [ "$apt" ]
+then
+    package_manager=$apt
+else
+    echo "ERROR! Unsupported Linux distribution (must be Debian or Red Hat)."
+    exit 1
+fi
+
+function install_package {
+    while true; do
+        read -p "Do you wish to install $1? (y/n) " yn
+        case $yn in
+            [Yy]* ) sudo $package_manager -y install $1; break;;
+            [Nn]* ) exit 1;;
+            * ) echo "Please answer y(es) or n(o).";;
+        esac
+    done
+}
+
 if ! [ $(command -v python3) ]
 then
     echo "ERROR! Python 3 is not found."
-    exit 1
+    install_package python3
 fi
 
 if ! [ $(command -v pip3) ]
 then
     echo "ERROR! Python 3 package manager (PIP) is not found."
-    exit 1
+    install_package python3-pip
 fi
 
 python3 -m pip install --upgrade pip
